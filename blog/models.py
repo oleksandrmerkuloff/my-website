1d4f7f34e0729c4963b65ade0070d6722d4f9d68
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-11-20 01:52:08 +0200

    had created models and had some fixes in settings.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
+from typing import Iterable
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    short_desc = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='posts/%Y/%m/%d/')
+    content = models.TextField()
+    slug = models.SlugField()
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def save(self, force_insert: bool = ..., force_update: bool = ..., using: str | None = ..., update_fields: Iterable[str] | None = ...) -> None:
+        if not self.id:
+            self.slug = slugify(self.title)
+        return super().save(force_insert, force_update, using, update_fields)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    short_desc = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='projects/%Y/%m/%d/')
+    content = models.TextField()
+    slug = models.SlugField()
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def save(self, force_insert: bool = ..., force_update: bool = ..., using: str | None = ..., update_fields: Iterable[str] | None = ...) -> None:
+        if not self.id:
+            self.slug = slugify(self.title)
+        return super().save(force_insert, force_update, using, update_fields)
--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +124,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
