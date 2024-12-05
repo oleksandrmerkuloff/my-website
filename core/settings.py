cd4511097a48a06a9fd83e4ba95f6d265cc47a94
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-12-05 10:54:19 +0200

    base html file and style for it

--- core/settings.py
+++ core/settings.py
@@ -132,3 +132,7 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
