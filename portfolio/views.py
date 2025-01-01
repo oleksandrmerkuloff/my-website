56aed00ede11acc9f07a15fab7cfe0821e041d52
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-01-01 23:46:05 +0200

    connect bootstrap and fix footer bug

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,11 +6,10 @@ from .models import Project
 
 def portfolio_page(request):
     projects = Project.objects.all()
-    return HttpResponse('Project')
-    # return render(request, 'portfolio/portfolio_page.html',
-    #               {
-    #                   'projects': projects
-    #               })
+    return render(request, 'portfolio/portfolio_page.html',
+                  {
+                      'projects': projects
+                  })
 
 
 def single_project(request, project_id):
