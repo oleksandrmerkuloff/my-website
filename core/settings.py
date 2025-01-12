716d54b670c34c1826024832c87a76701f63c412
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-01-12 22:08:35 +0200

    add requirements

--- core/settings.py
+++ core/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 
 database_url = os.environ.get('DATABASE_URL')
-DATABASES['default'] = dj_database_url.parse(database_url) 
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
